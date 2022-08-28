aa0f56477ee1cc76dae4739599aa8a14837501a4
Kelly Hum <kellyhum88@gmail.com>
2022-08-28 15:20:57 -0700

    switch hannah's name to jane doe

--- my-app/src/components/Nav/Nav.js
+++ my-app/src/components/Nav/Nav.js
@@ -14,13 +14,13 @@ const Nav = () => {
 
   return (
     <div id='nav'>
-      <h1>🍳Welcome, Hannah!</h1>
+      <h1>🍳Welcome,Jane!</h1>
 
       <div className='nav-right-side'>
         <h2 className='nav-date-time'>{currentTime.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})} | {currentTime.toLocaleDateString([], {year: 'numeric', month: 'long', day: 'numeric'})}</h2>
 
         <div className='profile'>
-          <h3>Hannah Li</h3>
+          <h3>Jane Doe</h3>
           <img className='profile-image' src='https://images.unsplash.com/photo-1524650359799-842906ca1c06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80' alt='' />
         </div>
       </div>
