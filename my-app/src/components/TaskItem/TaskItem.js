5281692c874526628dc861318c27f4e5a5a6f00f
Kelly Hum <kellyhum88@gmail.com>
2022-08-28 13:26:40 -0700

    add checkbox selector (not working)

--- my-app/src/components/TaskItem/TaskItem.js
+++ my-app/src/components/TaskItem/TaskItem.js
@@ -1,6 +1,18 @@
 import './TaskItem.css'
 
 const TaskItem = ({task}) => {
+    const checkboxes = document.querySelectorAll('input[type=checkbox]')
+
+    console.log(checkboxes)
+
+    checkboxes.forEach((checkbox, index) => {
+        if (checkbox.checked) {
+            console.log('working', index)
+        } else {
+            console.log('not clicked', index)
+        }
+    })
+
     return ( 
         <div className={task.completed ? 'taskitem completed' : 'taskitem'}> {/** if task is checked, apply class 'taskitem completed'.  if not, class = 'taskitem' */}
             <h3>{task.name}</h3>
