1ac1b5bd45ffcd50a8873ff4fd80fe86f6b110bd
Kelly Hum <kellyhum88@gmail.com>
2022-08-28 09:35:25 -0700

    switch spotify playlist

--- my-app/src/components/Music/Music.js
+++ my-app/src/components/Music/Music.js
@@ -3,7 +3,7 @@ import './Music.css';
 const Music = () => {
     return ( 
         <div className='music widgets'>
-            <iframe className='spotify' title='Spotify playlist' src="https://open.spotify.com/embed/playlist/5VNJY2hce1SgJnYv0XHlor?utm_source=generator&theme=0" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+            <iframe className='spotify' title='Spotify playlist' src="https://open.spotify.com/embed/playlist/0vvXsWCC9xrXsKd4FyS8kM?utm_source=generator&theme=0" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
         </div>
     );
 }
